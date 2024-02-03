ff810d94c1fdedfe9c73bfc0c5b9771f19c359a0
Boldizsar Szabo <szboldisz@gmail.com>
2024-02-03 23:03:13 +0100

    Modify ft_strjoin

--- pipex/libft/src/ft_strjoin.c
+++ pipex/libft/src/ft_strjoin.c
@@ -12,46 +12,40 @@
 
 #include "../include/libft.h"
 
-// concatenate s1 and s2 into s_final
-static char	*concat_strs(char *s1, char *s2, char *s_final)
+// check if s1 and s2 are valid
+// return 0 if valid, otherwise return -1
+static int	check_strs(char *s1, char *s2)
 {
-	int	i;
-	int	j;
+	if (s1 == NULL)
+		return (-1);
+	if (s2 == NULL)
+		return (-1);
+	return (0);
+}
+
+// join s1 and s2 into a new string with malloc and return it
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*s_final;
 
 	i = -1;
+	j = 0;
+	if (check_strs(s1, s2) == -1)
+		return (NULL);
+	s_final = (char *)malloc(((ft_strlen(s1) + ft_strlen(s2)) + 1)
+		* sizeof(char));
+	if (s_final == NULL)
+		return (NULL);
 	while (s1[++i])
 		s_final[i] = s1[i];
-	j = 0;
 	while (s2[j])
 		s_final[i++] = s2[j++];
 	s_final[i] = '\0';
 	return (s_final);
 }
 
-// join s1 and s2 into a new string with malloc and return it
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*s_final;
-
-	if (!s1)
-	{
-		s1 = (char *)malloc(sizeof(char));
-		if (!s1)
-			return (NULL);
-		s1[0] = '\0';
-	}
-	if (!s2)
-		return (NULL);
-	s_final = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (s_final == NULL)
-	{
-		free(s1);
-		return (NULL);
-	}
-	s_final = concat_strs(s1, s2, s_final);
-	free(s1);
-	return (s_final);
-}
 /*
 #include <stdio.h>
 int main(void)
